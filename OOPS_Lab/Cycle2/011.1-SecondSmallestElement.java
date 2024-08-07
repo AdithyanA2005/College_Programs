2ee47fcb88b0cb481c8c75b0ffddc26a971c8a8a
Adithyan A <deepaadithyan56@gmail.com>
2024-08-07 19:15:07 +0530

    Add OOPS Lab program to find the second smallest element in an array

---
+++ OOPS_Lab/Cycle2/011.1-SecondSmallestElement.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class SecondSmallestElement1 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter array size: ");
+    int size = scanner.nextInt();
+    int[] arr = new int[size];
+
+    System.out.print("Enter array elements: ");
+    for (int i = 0; i < size; i++) {
+      arr[i] = scanner.nextInt(); 
+    }
+
+    int smallest = arr[0];
+    int secondSmallest = arr[0];
+
+    for (int i = 0; i < size; i++) {
+      if (arr[i] < smallest) {
+        secondSmallest = smallest;
+        smallest = arr[i]; 
+      } else if (arr[i] < secondSmallest && arr[i] > smallest) {
+        secondSmallest = arr[i];
+      }
+    } 
+
+    System.out.print("The second smallest element is " + secondSmallest);
+  }
+}
